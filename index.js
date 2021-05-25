85e85b20bc684ac728b3eaaa7250939e4e1e1d02
Rituraj Datta <83709983+riturajcodes@users.noreply.github.com>
2021-05-25 15:50:37 +0530

    update

--- index.js
+++ index.js
@@ -5,27 +5,21 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-const { v4: uuidv4 } = require("uuid");
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
-app.use("/public", express.static("public"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 app.get("/room", (req, res) => {
-  res.redirect(`/room/${uuidv4()}`);
+  res.render("chat");
 });
-app.get("/room/:id", (req, res) => {
-  res.render("chat", { roomId: req.params.id });
-});
-
 // socket io code
 
 io.on("connection", (socket) => {
-  socket.on("join-room", (roomId, userId) => {
-    socket.join(roomId);
+  socket.on("join-room", (userId) => {
     socket.broadcast.emit("user-connected", userId);
     socket.on("chat message", (msg) => {
       socket.broadcast.emit("chat message", msg);
