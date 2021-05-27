45ab43ca4437c43eeca9eddac8277274b422581a
Rituraj Datta <83709983+riturajcodes@users.noreply.github.com>
2021-05-28 00:01:29 +0530

    version 2.0.0

---
+++ public/js/app.js
@@ -0,0 +1,53 @@
+var socket = io("/");
+// socket io code
+
+let messages = document.getElementById("messageContainer");
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let sound = document.getElementById("sound");
+let username = prompt("Enter your name to join", "guest");
+console.log(ROOM_ID);
+
+socket.emit("join-room", ROOM_ID);
+
+socket.on("user-connected", (userId) => {
+  console.log("new " + userId);
+});
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit("chat message", input.value, username);
+    input.value = "";
+  }
+});
+socket.on("chat message", function (msg) {
+  var item = document.createElement("div");
+  item.textContent = msg;
+  item.classList.add("left");
+  item.classList.add("message");
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+socket.on("user-connected-msg", () => {
+  console.log("user joined");
+  var item = document.createElement("div");
+  item.innerText = `A new user just showed up in this chat`;
+  item.classList.add("left");
+  item.classList.add("message");
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+// socket io code
+let sendBtn = document.getElementById("sendBtn");
+sendBtn.addEventListener("click", () => {
+  if (input.value != "") {
+    var item = document.createElement("div");
+    item.innerText = input.value;
+    item.classList.add("right");
+    item.classList.add("message");
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+    sound.play();
+  }
+});
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const { v4: uuidv4 } = require("uuid");
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(require("cors")());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/start", (req, res) => {
+  res.render("start");
+});
+app.get("/room", (req, res) => {
+  res.redirect(`/room/${uuidv4()}`);
+});
+app.get(`/room/:roomId`, (req, res) => {
+  res.render("chat", { roomId: `/room/${req.params.roomId}` });
+});
+
+// socket io code
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", "user");
+    socket.to(roomId).emit("user-connected-msg");
+    socket.on("chat message", (msg) => {
+      socket.to(roomId).emit("chat message", msg);
+    });
+  });
+});
+// socket io code
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`server started at http://localhost:${PORT}`);
+});
